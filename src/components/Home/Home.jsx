9784484227fc9a79f5df885582bd7a3e391b742f
MathisDS15 <mathisds1506@gmail.com>
2025-09-25 11:16:03 +0100

    I created my home page with my profile picture and some text.

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,7 +6,13 @@ function Home() {
     return (
         <div className="home">
             <div className="home-container">
-
+                <div className="home-picture">
+                    <img src="/images/pp_circle.svg" className="home-profile-picture" alt="" />
+                </div>
+                <div className="home-text">
+                    <h1>Hi, I am Mathis ! </h1>
+                    <h2>I am currently looking for a professional contract as <br /> a Full-Stack engineer or Software engineer</h2>
+                </div>
             </div>
         </div>
     )
