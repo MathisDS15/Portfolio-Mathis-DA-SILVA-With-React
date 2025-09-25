7392131bce4b28ad7465fdaa9044f79ca346fc5a
MathisDS15 <mathisds1506@gmail.com>
2025-09-25 16:42:24 +0100

    I added several information in my 'about' page.

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -13,37 +13,30 @@ function About () {
                         <h3>Learn to know me a little better</h3>
                     </div>
                     <div className="about-grid-subbox">
-                        <div className="about-subbox-1">
-                            <div className="about-subbox-1-card-1">
-
+                        <div className="about-subbox">
+                            <div className="about-subbox-card-1">
+                                <h4>Who am I ?</h4>
+                                <p>
+                                    I am Mathis, an engineering student. I am 22 years-old. 
+                                    I have had a passion for programming since the age of 15. 
+                                    Very early on, I knew that I wanted to become a computer engineer, 
+                                    and today I'm actively pursuing this project.
+                                </p>
                             </div>
-                            <div className="about-subbox-1-card-2">
-
+                            <div className="about-subbox-card-2">
+                                <h4>Education</h4>
+                                <li></li>
+                                <li></li>
+                                <li></li>
                             </div>
-                            <div className="about-subbox-1-card-3">
-
-                            </div>
-                        </div>
-                        <div className="about-subbox-2">
-                            <div className="">
-
-                            </div>
-                            <div className="">
-
-                            </div>
-                            <div className="">
-
-                            </div>
-                        </div>
-                        <div className="about-subbox-3">
-                            <div className="about-subbox-3-card-1">
-
-                            </div>
-                            <div className="about-subbox-3-card-2">
-
-                            </div>
-                            <div className="about-subbox-3-card-3">
-
+                            <div className="about-subbox-card-3">
+                                <h4>Who do I want to be ?</h4>
+                                <p>
+                                   I want to be an engineer in computer science, yes ! 
+                                    It's not a surprise. However, who do I want to be for 
+                                    you ? I think I could be the next one among you. I would 
+                                    ensure that I carry out the tasks entrusted to me efficiently. 
+                                </p>
                             </div>
                         </div>
                     </div>
